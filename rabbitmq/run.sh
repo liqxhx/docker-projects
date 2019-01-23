e1bc437523d2b694bf7dbac2df302f23bfa1752a
liqxhx <liqxhx@gmail.com>
2019-01-23 17:17:45 +0800

    Update run.sh
    修改rabbitmq/run.sh

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,2 +1,3 @@
-docker run -d --hostname my-rabbit --name rbtmq_mgmt -p 15672:15672 -p 25672:25672 rabbitmq:3.7.8-management
-docker run -d --hostname my-rabbit --name rbtmq -p 5671:5671 -p 5672:5672 rabbitmq:3.7.8
+# docker run -d --hostname my-rabbit --name rbtmq_mgmt -p 15672:15672 -p 25672:25672 rabbitmq:3.7.8-management
+# docker run -d --hostname my-rabbit --name rbtmq -p 5671:5671 -p 5672:5672 rabbitmq:3.7.8
+docker run -d --hostname my-rabbit --name rbtmq -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=admin -p 15672:15672 -p 5672:5672 -p 25672:25672 -p 61613:61613 -p 1883:1883 rabbitmq:3.7.8-management
